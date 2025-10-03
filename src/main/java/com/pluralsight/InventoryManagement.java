c2756fc7bc62d585142c60a23d4ef579a1a187ae
montasia harris <mharris@my.yearupunited.com>
2025-10-03 15:41:48 -0400

    wrkbkwk2 library exercise

--- src/main/java/com/pluralsight/InventoryManagement.java
+++ src/main/java/com/pluralsight/InventoryManagement.java
@@ -1,15 +1,16 @@
 package com.pluralsight;
 
 import java.util.Scanner;
+import java.util.zip.CheckedOutputStream;
 
 public class InventoryManagement {
     public static void main(String[] args) {
 
         Book[] inventory = {
-                 new Book(1, "978-0439139601", "Harry Potter and the Goblet of Fire", "",false),
+                new Book(1, "978-0439139601", "Harry Potter and the Goblet of Fire", "", false),
                 new Book(2, "978-0141439600", "Pride and Prejudice", "Jane Austen", true),
-                 new Book(3, "978-0553380163", "A Game of Thrones", "",false),
-                 new Book(4, "978-0061120084", "To Kill a Mockingbird", "Harper Lee", false),
+                new Book(3, "978-0553380163", "A Game of Thrones", "", false),
+                new Book(4, "978-0061120084", "To Kill a Mockingbird", "Harper Lee", false),
                 new Book(5, "978-0451524935", "1984", "George Orwell", true),
         };
         Scanner input = new Scanner(System.in);
@@ -28,20 +29,35 @@ public class InventoryManagement {
             input.nextLine();
 
             switch (userChoice) {
-                case 1: //Show Available Books
-                    //showAvailableBooks()
+                case 1: System ShowAvailableBooks;
+                System.out.println("ShowAvailableBooks");
                     break;
-                case 2: //Show Checked Out Books
-                    //showCheckedOutBooks()
+                case 2: System ShowCheckedOutBooks;
+                System.out.println("showCheckedOutBooks");
                     break;
-                case 3: //Exit Program
+                case 3: System ExitProgram;;
                     input.close();
                     System.exit(0);
                     break;
-                default: //Invalid Choice
-                    System.out.println("Invalid Choice, try again...");
-
+                default: System.out.println("Book is checked out");
+                    System.out.println("Book is not checked out...");
             }
         }
     }
-}
+
+                    public static void ShowavAilibleBooks(Book[]Book){
+        for(Book Inventory :Book) {
+
+
+            System findCheckedOutBooks;; (Book[]Book Inventory;= InventoryManagement;){
+                for (Book book : Inventory) {
+                    if (book.isCheckedOut()) {
+                        System.out.println("Checked Out Book: " + book.getTitle() + " (id: " + book.getid() + ")");
+
+                    }
+
+                }
+            }
+        }
+
+
